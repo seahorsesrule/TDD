335483341520834546bd6b38d4d99ed16a5c1fea
dab <forsythia@gmail.com>
2014-08-29 16:04:39 -0400

    Updated functional tests to prepare for additional users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,6 +40,8 @@ class NewVisitorTest(LiveServerTestCase):
         # When they hit enter, the page updates, and now the page lists
         # "1. Visit North Korea 1948" as a to-do list item
         inputbox.send_keys(Keys.ENTER)
+        billted_list_url = self.browser.current_url
+        self.assertRegex(billted_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1. Visit North Korea 1948')
 
         # There is still a text-box for them to enter another item. They enter,
@@ -52,10 +54,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1. Visit North Korea 1948')
         self.check_for_row_in_list_table('2. Assassinate Kim Il-sung')
 
-        # Bill and Ted wonder whether the site will remember their list. Then they see that
-        # the site has generated a unique URL for them -- there is some explanatory text to that effect
-        self.fail('Finish the test!')
+        # Now a new user, Rufus, comes along to the site.
 
-        # They visit the URL - their to-do list is still there.
+        ## We use a new browser session to make sure that no information of
+        ## Bill and Ted is coming through from cookies et. #
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        # Satisfied, they party on.
+        # Rufus visits the home page. There is no sign of Bill and Ted's list.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Visit North Korea 1948', page_text)
+        self.assertNotIn('Assassinate Kim Il-sung', page_text)
+
+        # Rufus starts a new list by entering a new item.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Be excellent to each other')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Rufus gets his own unique URL
+        rufus_list_url = self.browser.current_url
+        self.assertRegex(rufus_list_url, '/lists/.+')
+        self.assertNotEqual(rufus_list_url, billted_list_url)
+
+        # Again, there is no trace of Bill and Ted's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Visit North Korea 1948', page_text)
+        self.assertIn('Be excellent to each other', page_text)
+
+        # Satisfied, they all party on.
