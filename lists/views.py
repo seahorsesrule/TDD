2f729539d4d266227f913e14aa4a86fd4977b550
dab <forsythia@gmail.com>
2014-09-02 16:49:25 -0400

    New URL + view for adding to exisitng lists. FT passes

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,16 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items':items})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create() # creates a new list
     Item.objects.create(text=request.POST['item_text'], list=list_) # adds the text the user entered to the list
-    return redirect('/lists/the-only-list-in-the-world/') # redirects to the list page
+    return redirect('/lists/%d/' % (list_.id,)) # redirects to the list page
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect ('/lists/%d/' % (list_.id,))
--- superlists/urls.py
+++ superlists/urls.py
@@ -6,10 +6,13 @@ urlpatterns = patterns('',
         'lists.views.home_page',
         name='home'
         ),
-    url(r'^lists/the-only-list-in-the-world/$',
+    url(r'^lists/(\d+)/$',
         'lists.views.view_list',
         name='view_list'
         ),
+    url(r'^lists/(\d+)/add_item$',
+        'lists.views.add_item',
+        name='add_item'),
     url(r'^lists/new$',
         'lists.views.new_list',
         name='new_list'
