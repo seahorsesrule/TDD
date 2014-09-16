3ab50eb27d936a36d58a8761dfc1a7d8472f0fef
dab <forsythia@gmail.com>
2014-09-16 19:13:32 -0400

    Move unit tests into a folder with single file

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,9 +1,7 @@
 from .base import FunctionalTest
-from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
-    @skip
     def test_cannot_add_empty_list_items(self):
         # Bill and Ted go to the homepage and accidentally try to submit
         # an empty list item. They hit enter on the empty input box.
