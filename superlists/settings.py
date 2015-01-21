35f54538953e9d5c089cf442b6b3d2d0dde23c3f
dab <forsythia@gmail.com>
2015-01-21 15:21:47 -0500

    Update settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/Desktop/goatnoises/secret_key.txt') as f:
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
